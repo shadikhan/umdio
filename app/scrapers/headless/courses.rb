93a75b12392af95fdf3aa93750ccb303e9f87c4c
Evan McIntire <mcintire.evan@gmail.com>
2018-05-22 21:44:09 -0700

    Begin headless browser scraper
    * Start new scrapers based on headless browsers
    * Currently visits every department, expands all sections, and prints
    the class test

---
+++ app/scrapers/headless/courses.rb
@@ -0,0 +1,31 @@
+require 'watir'
+
+def scrape_dep(browser)
+	browser.elements('class': 'toggle-sections-link-text').each do |elem|
+		elem.parent.click()
+	end
+
+	browser.elements('class': 'course').each do |elem|
+		puts elem.text
+	end
+end
+
+# Init headless browser
+browser = Watir::Browser.new :chrome,  headless: true
+browser.goto 'https://app.testudo.umd.edu/soc/'
+
+# Iterate over all the department links
+browser.elements(class: 'prefix-abbrev').each do |elem|
+	puts elem.parent.text
+	# Navigate to department
+	elem.parent.click
+
+	# Call scraper helper function
+	scrape_dep(browser)
+
+	# Return to SOC
+	browser.back
+end
+
+# Clean up
+browser.quit
