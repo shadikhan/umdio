59c61a686aea8cc4a95400be37fc6e41c6bd22e3
Evan McIntire <mcintire.evan@gmail.com>
2018-09-17 15:32:34 -0400

    Add postgres; code cleanup

--- app/scrapers/headless/courses.rb
+++ app/scrapers/headless/courses.rb
@@ -1,44 +1,54 @@
 require 'watir'
 require 'logger'
+require 'pg'
 
 @logger = Logger.new(STDOUT)
 @logger.level = Logger::WARN
 
-def scrape_dep(browser)
-	#browser.elements('class': 'toggle-sections-link-text').each do |elem|
-	#	elem.parent.click()
-	#end
-	b = 0
+def scrape_department(browser)
+	browser.elements('class': 'toggle-sections-link-text').each do |elem|
+		browser.execute_script('arguments[0].scrollIntoView();', elem)
+		elem.click()
+	end
+
 	browser.elements('class': 'course').each do |elem|
 		@logger.info(elem.text)
-		b += 1
 	end
 end
 
-# Init headless browser
-browser = Watir::Browser.new :chrome,  headless: true, url: "http://chromedriver:4444", :switches => ["disable-infobars", "no-sandbox"]
-browser.goto 'https://app.testudo.umd.edu/soc/'
+def scrape_semester(semester)
+	# Create tables
+	sql = File.open('../../courses.sql', 'rb') { |file| file.read }
+	with_db do |db|
+	begin
+		db.exec(sql)
+	rescue PG::Error
+		#####
+	end
 
-@logger.info('test')
+	# Init headless browser
+	browser = Watir::Browser.new :chrome,  headless: true, url: "http://chromedriver:4444", :switches => ["disable-infobars", "no-sandbox"]
+	browser.goto 'https://app.testudo.umd.edu/soc/'
 
-start = Time.now
-# Iterate over all the department links
-browser.elements(class: 'prefix-abbrev').each do |elem|
-	@logger.info(elem.parent.text)
-	# Navigate to department
-	elem.parent.click
-	
-	# Call scraper helper function
-	scrape_dep(browser)
+	start = Time.now
 
-	# Return to SOC
-	browser.back
+	# Iterate over all the department links
+	browser.elements(class: 'prefix-abbrev').each do |elem|
+		@logger.info(elem.parent.text)
+		# Navigate to department
+		elem.parent.click
+
+		# Call scraper helper function
+		scrape_department(browser)
+
+		# Return to SOC
+		browser.back
+	end
+
+	finish = Time.now
+	diff = (finish - start)
+	puts "Took #{diff} seconds"
+
+	# Clean up
+	browser.quit
 end
-finish = Time.now
-
-diff = (finish - start)
-
-puts "Took #{diff} seconds"
-
-# Clean up
-browser.quit
